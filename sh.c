2d36ea1e03a9d496e2973c4648ffde965e0fb9e7
oradrok <oradrok@users.noreply.github.com>
2018-03-28 17:41:21 +0300

    add declaration yet again

--- sh.c
+++ sh.c
@@ -156,6 +156,7 @@ getcmd(char *buf, int nbuf)
 
 /************************************************************************************/
 void replaceSysVariables(char* buf);
+int isLetter(char* ch);
 
 //add all commands to history array
 //history will hold maximum MAX_HISTORY commands
